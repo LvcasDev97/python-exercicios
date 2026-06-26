2b3a0296280b6628e41011158c9a3bb5eb6751b1
Lucas <152229353+LvcasDev97@users.noreply.github.com>
2026-06-26 16:58:18 -0300

    Add files via upload
    Exercícios de pratica

---
+++ Grupo da Maioridade.py
@@ -0,0 +1,14 @@
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range(1,8):
+    nasc = int(input('Em que ano a pessoal nasceu?'))
+    idade = atual - nasc
+    print('Essa pessoa tem {} anos'.format(idade))
+    if idade >= 21:
+       totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo tivemos {} pessoas meiores de idade'.format(totmaior))
+print('É também tivemos {} pessoas menores de idade'.format(totmenor))
